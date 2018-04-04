2bab5caa43f4c48b75c4548d10adb1211a2004ef
unknown <jtc7537@gmail.com>
2018-04-03 22:51:57 -0400

    parsing content from cheerio

--- controllers/html.js
+++ controllers/html.js
@@ -8,9 +8,29 @@ module.exports = (app) => {
         request('https://www.nytimes.com/', (err,response,body) => {
             if(!err && response.statusCode == 200){
                var $ = cheerio.load(body);
-               $('p.summary').each(function(i, element){
-                   var desc = $(this);
-                   console.log(desc.text())
+               $('div.collection').each(function(i, element){
+                    var div = $(this)
+                    var title = div.next().children().text();
+                    var author = false;
+                    var desc = div.next().next().next().text();
+                    if(div.next().next().text().indexOf('By') >= 0){
+                        author = div.next().next().text()
+                    }
+                    
+                //    var desc = p.summary.text()
+                if(author){
+                    var story = {
+                        title:title.replace(/(\n)/gm,'').trim(),
+                        author:author.replace(/(\n)/gm,'').trim(),
+                        desc:desc.replace(/(\n)/gm,"").trim()
+                    }
+                    console.log(story)
+                }
+                //   console.log(author.text().indexOf('By'))
+                //   if(author.indexOf('By') >= 0){
+                   //   console.log(title, desc, author) 
+                //   }
+                   
                });
                
                 // console.log(body)
