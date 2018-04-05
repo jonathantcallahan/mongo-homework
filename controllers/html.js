712ae2c1f9ca689cfb73ae04431b75a02edab7ff
unknown <jtc7537@gmail.com>
2018-04-04 21:16:15 -0400

    cont work on cheerio

--- controllers/html.js
+++ controllers/html.js
@@ -1,42 +1,49 @@
 const path = require('path');
 const request = require('request');
 const cheerio = require('cheerio');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/news')
+const Schema = mongoose.Schema;
+const userDataSchema = new Schema({
+    title: String,
+    author: String,
+    desc: String
+})
+
+var Article = mongoose.model('Articles',userDataSchema)
 
 module.exports = (app) => {
     app.get('/', (req,res) => {
         request('https://www.nytimes.com/', (err,response,body) => {
             if(!err && response.statusCode == 200){
+
                var $ = cheerio.load(body);
-               $('div.collection').each(function(i, element){
-                    var div = $(this)
-                    var title = div.next().children().text();
-                    var author = false;
-                    var desc = div.next().next().next().text();
-                    if(div.next().next().text().indexOf('By') >= 0){
-                        author = div.next().next().text()
-                    }
+
+               $('h2.story-heading').each(function(i, element){
                     
-                //    var desc = p.summary.text()
-                if(author){
-                    var story = {
-                        title:title.replace(/(\n)/gm,'').trim(),
-                        author:author.replace(/(\n)/gm,'').trim(),
-                        desc:desc.replace(/(\n)/gm,"").trim()
+                    var h2 = $(this)
+                    let title = h2.children().text().trim();
+                    let by = h2.next().next().text().trim();
+                    let desc = h2.next().next().next().text().trim();
+                    
+                    
+                    if(by.indexOf('By') >= 0){
+
+                        var story = {
+                            title: title,
+                            author: by,
+                            desc: desc
+                        }
+
+                        console.log(story)
+
+                        var data = new Article(story)
+                        data.save()
                     }
-                    console.log(story)
-                }
-                //   console.log(author.text().indexOf('By'))
-                //   if(author.indexOf('By') >= 0){
-                   //   console.log(title, desc, author) 
-                //   }
-                   
                });
-               
-                // console.log(body)
             }
-            res.send(body)
+            res.sendFile(path.join(__dirname, './../public/homepage.html'))
         })
-        //res.sendFile(path.join(__dirname,'./../public/homepage.html'))
     })
 }
